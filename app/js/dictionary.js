68520016d6e04e94b3384283dee16f36b1c5cd7b
Emre Özcan <justsomechars@gmail.com>
2021-03-23 17:32:21 +0300

    Add a window that displays information about a word

--- app/js/dictionary.js
+++ app/js/dictionary.js
@@ -110,71 +110,71 @@ const Fields = {
 };
 
 const MeaningProperties = {
-    18: "ünlem (sözcük türü)",
-    19: "isim (sözcük türü)",
-    20: "sıfat (sözcük türü)",
-    21: "-e (sözcük türü)",
-    22: "-i (sözcük türü)",
-    23: "nesnesiz (sözcük türü)",
-    24: "zarf (sözcük türü)",
-    25: "-le (sözcük türü)",
-    26: "-den (sözcük türü)",
-    27: "edat (sözcük türü)",
-    28: "bağlaç (sözcük türü)",
-    29: "zamir (sözcük türü)",
-    30: "argo (üslup)",
-    31: "eskimiş (üslup)",
-    32: "mecaz (üslup)",
-    33: "halk ağzında (üslup)",
-    34: "teklifsiz konuşmada (üslup)",
-    35: "alay yollu (üslup)",
-    36: "kaba konuşmada (üslup)",
-    37: "şaka yollu (üslup)",
-    38: "hakaret yollu (üslup)",
-    39: "müzik (alan)",
-    40: "spor (alan)",
-    41: "bitki bilimi (alan)",
-    42: "denizcilik (alan)",
-    43: "tarih (alan)",
-    44: "gök bilimi (alan)",
-    45: "coğrafya (alan)",
-    46: "dil bilgisi (alan)",
-    47: "ruh bilimi (alan)",
-    48: "kimya (alan)",
-    49: "anatomi (alan)",
-    50: "ticaret (alan)",
-    51: "hukuk (alan)",
-    52: "matematik (alan)",
-    53: "hayvan bilimi (alan)",
-    54: "edebiyat (alan)",
-    55: "sinema (alan)",
-    56: "biyoloji (alan)",
-    57: "felsefe (alan)",
-    58: "fizik (alan)",
-    59: "tiyatro (alan)",
-    60: "jeoloji (alan)",
-    61: "teknik (alan)",
-    62: "toplum bilimi (alan)",
-    63: "fizyoloji (alan)",
-    64: "meteoroloji (alan)",
-    65: "mantık (alan)",
-    66: "ekonomi (alan)",
-    67: "mimarlık (alan)",
-    68: "mineraloji (alan)",
-    69: "eğitim bilimi (alan)",
-    73: "askerlik (alan)",
-    80: "geometri (alan)",
-    81: "teknoloji (alan)",
-    82: "yardımcı  fiil (sözcük türü)",
-    83: "-de (sözcük türü)",
-    84: "dil bilimi (alan)",
-    85: "tıp (alan)",
-    87: "televizyon (alan)",
-    88: "din bilgisi (alan)",
-    96: "madencilik (alan)",
-    98: "bilişim (alan)",
-    99: "mit. (alan)",
-    105: "antropoloji (alan)",
+    18: "ünlem",
+    19: "isim",
+    20: "sıfat",
+    21: "-e",
+    22: "-i",
+    23: "nesnesiz",
+    24: "zarf",
+    25: "-le",
+    26: "-den",
+    27: "edat",
+    28: "bağlaç",
+    29: "zamir",
+    30: "argo",
+    31: "eskimiş",
+    32: "mecaz",
+    33: "halk ağzında",
+    34: "teklifsiz konuşmada",
+    35: "alay yollu",
+    36: "kaba konuşmada",
+    37: "şaka yollu",
+    38: "hakaret yollu",
+    39: "müzik",
+    40: "spor",
+    41: "bitki bilimi",
+    42: "denizcilik",
+    43: "tarih",
+    44: "gök bilimi",
+    45: "coğrafya",
+    46: "dil bilgisi",
+    47: "ruh bilimi",
+    48: "kimya",
+    49: "anatomi",
+    50: "ticaret",
+    51: "hukuk",
+    52: "matematik",
+    53: "hayvan bilimi",
+    54: "edebiyat",
+    55: "sinema",
+    56: "biyoloji",
+    57: "felsefe",
+    58: "fizik",
+    59: "tiyatro",
+    60: "jeoloji",
+    61: "teknik",
+    62: "toplum bilimi",
+    63: "fizyoloji",
+    64: "meteoroloji",
+    65: "mantık",
+    66: "ekonomi",
+    67: "mimarlık",
+    68: "mineraloji",
+    69: "eğitim bilimi",
+    73: "askerlik",
+    80: "geometri",
+    81: "teknoloji",
+    82: "yardımcı  fiil",
+    83: "-de",
+    84: "dil bilimi",
+    85: "tıp",
+    87: "televizyon",
+    88: "din bilgisi",
+    96: "madencilik",
+    98: "bilişim",
+    99: "mit.",
+    105: "antropoloji",
 };
 
 let db;
@@ -409,6 +409,7 @@ function paginationPage(pageNumber) {
      */
 
     element_filteredWordPageHolder.innerHTML = "";
+    hidePeepWindow();
 
     let smallestForThisPage = itemsPerPage*(pageNumber - 1);
     let wordsOfThisPage = filteredWords.slice(smallestForThisPage, smallestForThisPage+itemsPerPage);
@@ -420,8 +421,7 @@ function paginationPage(pageNumber) {
     for (const word of wordsOfThisPage) {
         let listItem = document.createElement("li");
         listItem.innerText = word.entry;
-        listItem.id = `dictionary-${word.tdk_id}`;
-        listItem.class = "dictionary-word";
+        listItem.addEventListener("click", e => peep(e, word)); /* jshint ignore:line */
         fragment.appendChild(listItem);
     }
     element_filteredWordPageHolder.start = smallestForThisPage + 1;
@@ -431,4 +431,131 @@ function paginationPage(pageNumber) {
 
     element_filteredWordPageHolder.appendChild(fragment);
 
+}
+
+const element_wordInformation = document.getElementById("word-information");
+
+
+function peep(event, word) {
+    element_wordInformation.classList.remove("inactive");
+    element_wordInformation.style.top = `calc(${event.target.offsetTop}px + 1.5em)`;
+
+    element_wordInformation.innerHTML = "";
+
+    const fragment = document.createDocumentFragment();
+
+    const closeButton = document.createElement("button");
+    closeButton.classList.add("wordpeep-close-button");
+    closeButton.addEventListener("click", hidePeepWindow);
+    closeButton.innerHTML = "&times;";
+    fragment.appendChild(closeButton);
+
+
+    const title = document.createElement("h1");
+    title.classList.add("wordpeep-title");
+    if (word.order > 0) {
+        title.innerText = `${word.entry} (${word.order})`;
+    } else {
+        title.innerText = word.entry;
+    }
+    fragment.appendChild(title);
+
+    function adder(parent, tagName, field) {
+        if (field) {
+            const child = document.createElement(tagName);
+            child.classList.add("wordpeep-common-property");
+            child.innerText = field;
+            parent.appendChild(child);
+        }
+    }
+    const commonProperties = document.createElement("div");
+    commonProperties.classList.add("wordpeep-common-properties");
+
+    adder(commonProperties, "span", word.original);
+    adder(commonProperties, "span", word.pronunciation);
+    adder(commonProperties, "span", word.prefix);
+    adder(commonProperties, "span", word.suffix);
+
+    adder(commonProperties, "span", word.plural ? "çoğul" : false);
+    adder(commonProperties, "span", word.proper ? "özel" : false);
+    fragment.appendChild(commonProperties);
+
+    const meaningsList = document.createElement("ol");
+    meaningsList.classList.add("wordpeep-meanings-list");
+
+    for (const meaning of word.meanings) {
+        const meaningElement = document.createElement("li");
+        meaningElement.classList.add("wordpeep-meaning");
+
+        const meaningText = document.createElement("p");
+        meaningText.classList.add("wordpeep-meaning-text");
+        meaningText.innerText = meaning.meaning;
+        meaningElement.appendChild(meaningText);
+
+        if (meaning.properties.length !== 0) {
+            const meaningPropertiesContainer = document.createElement("ul");
+            meaningPropertiesContainer.classList.add("wordpeep-meaning-properties-list");
+            for (const property of meaning.properties) {
+                const meaningPropertyElement = document.createElement("li");
+                meaningPropertyElement.classList.add("wordpeep-meaning-property");
+                meaningPropertyElement.innerText = MeaningProperties[property];
+                meaningPropertiesContainer.appendChild(meaningPropertyElement);
+            }
+            meaningElement.appendChild(meaningPropertiesContainer);
+        }
+
+        if (meaning.examples.length !== 0) {
+            const examplesTitle = document.createElement("h2");
+            examplesTitle.classList.add("wordpeep-examples-title");
+            examplesTitle.innerText = "Örnek kullanımlar";
+            meaningElement.appendChild(examplesTitle);
+
+            const examplesContainer = document.createElement("ol");
+            examplesContainer.classList.add("wordpeep-examples-list");
+            for (const example of meaning.examples) {
+                const exampleElement = document.createElement("li");
+                exampleElement.classList.add("wordpeep-example");
+
+                const exampleText = document.createElement("span");
+                exampleText.classList.add("wordpeep-example-text");
+                exampleText.innerText = example.example;
+                exampleElement.appendChild(exampleText);
+
+                if (example.writer) {
+                    const writerName = document.createElement("span");
+                    writerName.classList.add("wordpeep-writer-name");
+                    writerName.innerText = example.writer.full_name;
+                    exampleElement.appendChild(writerName);
+                }
+                examplesContainer.appendChild(exampleElement);
+            }
+            meaningElement.appendChild(examplesContainer);
+        }
+
+        meaningsList.appendChild(meaningElement);
+    }
+    fragment.appendChild(meaningsList);
+
+    if (word.proverbs.length !== 0) {
+        const proverbsTitle = document.createElement("h2");
+        proverbsTitle.classList.add("wordpeep-proverbs-title");
+        proverbsTitle.innerText = "Atasözleri, deyimler ve birleşik sözcükler";
+        fragment.appendChild(proverbsTitle);
+
+        const proverbsList = document.createElement("ol");
+        proverbsList.classList.add("wordpeep-proverbs-list");
+        for (const proverb of word.proverbs) {
+            const proverbElement = document.createElement("li");
+            proverbElement.classList.add("wordpeep-proverb");
+            proverbElement.innerText = proverb.proverb;
+            proverbsList.appendChild(proverbElement);
+        }
+        fragment.appendChild(proverbsList);
+    }
+
+    element_wordInformation.appendChild(fragment);
+}
+
+function hidePeepWindow() {
+    element_wordInformation.classList.add("inactive");
 }
