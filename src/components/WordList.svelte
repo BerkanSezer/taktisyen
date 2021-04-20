04f53d6fc96bceef43e8acf36ef4c7c6867b1935
Emre Özcan <justsomechars@gmail.com>
2021-04-20 16:21:56 +0300

    Re-add word listing and pagination

--- src/components/WordList.svelte
+++ src/components/WordList.svelte
@@ -1,11 +1,48 @@
 <script>
     import {dictionary} from "../stores/dictionary.js";
+    import Word from "./Word.svelte";
+    import {paginate, PaginationNav} from "svelte-paginate";
+    import Paginator from "./Paginator.svelte";
+
+    let page = 1;
+    let pageSize = 100;
+
+    $: paginatedItems = paginate({items: $dictionary.filtered, pageSize: pageSize, currentPage: page});
 </script>
 
 <style lang="scss">
+    li {
+        cursor: help;
+    }
 
+    ol {
+        border-radius: 5px;
+        border: 2px groove lightgray;
+        padding: 0.2em 0.4em 0.2em 4em;
+    }
+
+    .words-holder {
+        margin-top: 2.5em;
+    }
 </style>
 
-<div>
-    {$dictionary.filtered.length} kelime bu filtrelere uyuyor.
+<div class="words-holder">
+    <Paginator
+            totalItems={$dictionary.filtered.length}
+            pageSize={pageSize}
+            currentPage={page}
+            limit={3}
+            showStepOptions={true}
+            on:setPage={(e) => page = e.detail.page}
+    />
+
+    <div class="listing-container">
+        <ol start={(page - 1)*pageSize + 1}>
+            {#each paginatedItems as word}
+                <li>
+                    <Word word={word}/>
+                </li>
+            {/each}
+        </ol>
+    </div>
 </div>
